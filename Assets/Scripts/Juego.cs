6788f9bde9ed891d26d9d6a43f61f6494bcd121e
Jose Gamboa <jgamboa@kinedu.com>
2019-08-11 22:47:10 -0500

    First commit

---
+++ Assets/Scripts/Juego.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Juego : MonoBehaviour
+{
+
+    public GameObject Prefab;
+    public Transform pos;
+    GameObject player1, player2;
+
+    // Use this for initialization
+    void Start()
+    {
+        player1 = Instantiate(Prefab, pos.position, Quaternion.identity);
+        player1.GetComponent<mov>().horizontal="Horizontal";
+        player1.GetComponent<mov>().vertical = "Vertical";
+
+        player2 = Instantiate(Prefab, pos.position, Quaternion.identity);
+        player2.GetComponent<mov>().horizontal="Horizontal2";
+        player2.GetComponent<mov>().vertical="Vertical2";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/mov.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mov : MonoBehaviour
+{
+    public float speed;
+    public float speedG;
+    bool player = true; // true = player 1, false = player
+    public string horizontal, vertical;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("horizontal: "+horizontal);
+        //Debug.Log("vertical: " + vertical);
+        float axisX = Input.GetAxis(horizontal) * speedG;
+        float axisY = Input.GetAxis(vertical) * speed;
+        Vector3 velo = GetComponent<Rigidbody>().velocity;
+
+        if (transform.position.y < -1.4f)
+            GetComponent<Rigidbody>().velocity = new Vector3(velo.x, 2.5f, velo.z);
+
+        if (Input.GetKey("up") || Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("down"))
+            transform.Translate(new Vector3(axisY, velo.y, velo.z)*Time.deltaTime);
+        
+        
+        if(Input.GetKey("left") || Input.GetKey("right") || Input.GetKey("a") || Input.GetKey("d"))
+            transform.Rotate(new Vector3(0, axisX, 0));
+        
+    }
+}
